c5c42e349e5b84d1b50f01bb1579a660a5835b0a
Ben G <beng545455@gmail.com>
2024-10-29 10:43:30 -0400

    Risk tables fixes + stratified grahs

--- R/ci_riskFinal.R
+++ R/ci_riskFinal.R
@@ -27,7 +27,7 @@
 #' #Plot it
 #' ci_risk(ajfit)
 #' 
-ci_risk <- function(model_name, factor_order = NULL, text_size = 4,xlab_name = "Time", ylab_name = "State") {
+ci_risk <- function(model_name, factor_order = NULL, text_size = 4, xlab_name = "Time", ylab_name = "State") {
   #Check if missing model
   if(missing(model_name)) {
     "You must input a survfit model for model_name"
@@ -38,64 +38,74 @@ ci_risk <- function(model_name, factor_order = NULL, text_size = 4,xlab_name = "
     stop("Model must be a competing risks model. See ?survfit.formula to see how to make that model")
   }
   
-dat2 <- broom::tidy(model_name) %>% dplyr::group_by(state) %>%
-  dplyr::mutate(csm = cumsum(n.event))
-
-brks <- pretty(dat2$time)
-brks <- brks[brks <= max(dat2$time)]
-#Get values that are the closest to breakpoints but not past them
-minz <- c()
-minz_past_point <- c()
-for (i in brks) {
+  dat2 <- broom::tidy(model_name) %>% dplyr::group_by(state) %>%
+    dplyr::mutate(csm = cumsum(n.event))
   
-  #The purpose of the second ifelse is to make sure that we don't end up with
-  #two of the same times if there are no events past the second breakpoint
-  minz <- c(minz,ifelse(i==0,min(dat2$time[dat2$time >= i]),
-                        ifelse(min(dat2$time[dat2$time >= 0]) == max(dat2$time[dat2$time <= i]),
-                               min(dat2$time[dat2$time >= i]),max(dat2$time[dat2$time <= i]))))
-  #Create column for this as well
-  minz_past_point <- c(minz_past_point,
-                       base::suppressWarnings(ifelse(min(dat2$time[dat2$time >= 0]) == max(dat2$time[dat2$time <= i]),
-                                               TRUE,FALSE)))
-  
-}
-#Subtract 1 from csm if its a past breakpoint time
-dat3 <- dat2 %>% dplyr::filter(time %in% minz) %>% dplyr::mutate(minz_past_point = minz_past_point) %>%
-  dplyr::mutate(csm = dplyr::case_when(
-    n.event == 1 & minz_past_point == TRUE ~ csm - 1,
+  brks <- pretty(dat2$time)
+  # brks <- brks[brks <= max(dat2$time)]
+  #Get values that are the closest to breakpoints but not past them
+  minz <- c()
+  minz_past_point <- c()
+  for (i in brks) {
+    
+    #The purpose of the second ifelse is to make sure that we don't end up with
+    #two of the same times if there are no events past the second breakpoint
+    minz <- c(minz,ifelse(i==0,min(dat2$time[dat2$time >= i]),
+                          ifelse(min(dat2$time[dat2$time >= 0]) == max(dat2$time[dat2$time <= i]),
+                                 min(dat2$time[dat2$time >= i]),max(dat2$time[dat2$time <= i]))))
+    #Create vector for this as well
+    minz_past_point <- c(minz_past_point,
+                         base::suppressWarnings(ifelse(min(dat2$time[dat2$time >= 0]) == max(dat2$time[dat2$time <= i]),
+                                                       TRUE,FALSE)))
+    
+  }
+  #past points
+  intr <- data.frame(minz, minz_past_point) %>% dplyr::filter(minz_past_point == TRUE) %>% pull(.,minz)
+  #Subtract 1 from csm if its a past breakpoint time
+  dat3 <- dat2 %>% dplyr::filter(time %in% minz) %>% dplyr::mutate(minz_past_point = time %in% minz_past_point) %>%
+    dplyr::mutate(csm = dplyr::case_when(
+      n.event == 1 & minz_past_point == TRUE ~ csm - 1,
+      TRUE ~ csm
+    )) %>%
+    #Add 1 to number at risk if its past a breakpoint time
+    dplyr::mutate(n.risk = dplyr::case_when(
+      minz_past_point == TRUE ~ n.risk + 1,
+      TRUE ~ n.risk
+    ))
+  #Set cumsum to zero for the non overall risk because no events actually happened at time 0
+  #Unless there is an event actually at 0
+  #Verified
+  dat3 <- dat3 %>% dplyr::mutate(csm = dplyr::case_when(
+    time == min(time, na.rm = TRUE) & min(time, na.rm = TRUE) != 0 ~ 0,
     TRUE ~ csm
-  )) %>%
-  #Add 1 to number at risk if its past a breakpoint time
-  dplyr::mutate(n.risk = dplyr::case_when(
-    minz_past_point == TRUE ~ n.risk + 1,
-    TRUE ~ n.risk
-  ))
-#Set cumsum to zero for the non overall risk because no events actually happened at time 0
-#Unless there is an event actually at 0
-#Verified
-dat3 <- dat3 %>% dplyr::mutate(csm = dplyr::case_when(
-  time == min(time, na.rm = TRUE) & min(time, na.rm = TRUE) != 0 ~ 0,
-  TRUE ~ csm
-)) %>% dplyr::mutate(state = ifelse(state == "(s0)","Number At Risk",state)) %>%
-  dplyr::mutate(state2 = factor(state, levels = c(dat3 %>%
-      dplyr::filter(state != "Number At Risk") %>% dplyr::pull(.,state) %>% base::unique(.),"Number At Risk"),
-                         ordered = TRUE)) %>%
-  #The purpose of this is to subtract the person that had the event at that time
-  #from the number of people at risk at that time. For zero, unless there is an an event
-  #at time zero, we don't modify it because no events at time zero happened
-  #and we want the total number of people at time zero unless an event happened
-  #at time zero.
-  dplyr::mutate(n.risk.new = ifelse(time == min(time,na.rm = TRUE) & min(time, na.rm = TRUE) != 0,
-                             n.risk,n.risk-1))
+  )) %>% dplyr::mutate(state = ifelse(state == "(s0)","Number At Risk",state)) %>%
+    dplyr::mutate(state2 = factor(state, levels = c(dat3 %>%
+                                                      dplyr::filter(state != "Number At Risk") %>%
+                                                      dplyr::pull(.,state) %>% base::unique(.),"Number At Risk"),
+                                  ordered = TRUE)) %>%
+    #The purpose of this is to subtract the person that had the event at that time
+    #from the number of people at risk at that time. For zero, unless there is an an event
+    #at time zero, we don't modify it because no events at time zero happened
+    #and we want the total number of people at time zero unless an event happened
+    #at time zero.
+    dplyr::mutate(n.risk.new = ifelse(time == min(time,na.rm = TRUE) & min(time, na.rm = TRUE) != 0,
+                                      n.risk,n.risk-1))
 #Make into a factor when told to
 if(!is.null(factor_order)) {
 new_fac <- c("Number At Risk")
 new_fac <- c(factor_order,new_fac)
 dat3$state2 <- factor(dat3$state2, levels = new_fac, ordered = TRUE)
 }
-
+  #Verified
+  #Set times to be equal to breaks 
+  #Get unique times
+  bn <- dat3 %>% arrange(time) %>% pull(.,time) %>% base::unique(.)
+  #Named vector with breaks and times
+  vvvv <- c(bn=brks)
+  #Column where the times correspond to the break times
+  dat3$new_time <- vvvv[as.factor(dat3$time)]
 ggplot2::ggplot(data = dat3,
-                ggplot2::aes(x=time,y=state2,label = ifelse(state == "Number At Risk",n.risk.new,csm))) + 
+                ggplot2::aes(x=new_time,y=state2,label = ifelse(state == "Number At Risk",n.risk.new,csm))) + 
   ggplot2::geom_text(size = text_size) +
   ggplot2::scale_x_continuous(breaks = brks) +
   ggplot2::labs(y=ylab_name, x = xlab_name) +
--- R/strat_ci_graph_final.R
+++ R/strat_ci_graph_final.R
@@ -1,4 +1,4 @@
-#' Create cumulative incidence plots
+#' Create stratified cumulative incidence plots
 #' 
 #' @importFrom dplyr %>%
 #'
@@ -6,7 +6,6 @@
 #' @param line_size A numeric containing the value of the line thickness. Default is 1.
 #' @param outcome_colors A vector consisting of the color names/hexadecimals. Default is colors from Dark 3 palette
 #' @param col_palette A string containing the name of the hcl palette to use. See hcl.pals() for names. Default is Dark 3
-#' @param factor_order A vector of the outcomes in the order that the user wants them to be listed in the legend. First vector will be at the top, second will be after it and so on and so forth. Please be careful and only use with a labeled factor of outcomes.
 #' @param xlab_name Label for the x-axis. Default is "Time"
 #' @param ylab_name Label for the y-axis. Default is "Cumulative Incidence"
 #' @param conf_int Determines whether a confidence interval should be drawn for each curve. Default is TRUE.
@@ -22,22 +21,21 @@
 #' Melanoma <- MASS::Melanoma
 #' library(survival)
 #' library(dplyr)
-#' #Make the variable in the format for competing risks
 #' Melanoma2 <- 
-#' Melanoma %>% 
-#' mutate(status = as.factor(recode(status, `2` = 0, `1` = 1, `3` = 2)))
+#'  Melanoma %>% 
+#'  mutate(status = as.factor(recode(status, `2` = 0, `1` = 1, `3` = 2)))
 #' #Make the status a labeled factor
 #' Melanoma2 <- Melanoma2 %>% mutate(status = factor(status,levels = c("0","1","2"),
-#'    labels = c("Censored","Melanoma","Other Causes")))
+#'          labels = c("Censored","Melanoma","Other Causes"))) %>%
+#'  mutate(sex = factor(sex,levels = c("0","1"), labels = c("Female","Male")))
 #' #Define the model
-#' ajfit <- survfit(Surv(time, status) ~1, data = Melanoma2)
+#' ajfit <- survfit(Surv(time, status) ~sex, data = Melanoma2)
 #' #Plot it
-#' ci_graph(ajfit)
+#' strat_ci_graph(ajfit)
 #' 
-ci_graph <- function(model_name, line_size = 1, outcome_colors=NULL, col_palette = "Dark 3",
-                     factor_order = NULL,
-                     xlab_name="Time",ylab_name="Cumlative Incidence", conf_int = TRUE,
-    conf_int_alpha = 0.2, legend_text_size = 10, legend_title_text_size = 12, legend_size = 0.5) {
+strat_ci_graph <- function(model_name, line_size = 1, outcome_colors = NULL, col_palette = "Dark 3",
+      xlab_name = "Time", ylab_name = "Cumlative Incidence", conf_int = TRUE, conf_int_alpha = 0.2,
+      legend_text_size = 10, legend_title_text_size = 12, legend_size = 0.5) {
   #Check if missing model
   if(missing(model_name)) {
     "You must input a survfit model for model_name"
@@ -47,16 +45,22 @@ ci_graph <- function(model_name, line_size = 1, outcome_colors=NULL, col_palette
   if(class(model_name)[1] != "survfitms" | class(model_name)[2] != "survfit") {
     stop("Model must be a competing risks model. See ?survfit.formula to see how to make that model")
   }
-  #Tidy the the data
-  dat2 <- broom::tidy(model_name)
-  #Make sure color palette exists
+  #Check if stratified
+  if("strata" %in% names(model_name) == FALSE) {
+    stop("Model must be stratified. Use ci_graph for unstratified analysis")
+  }
+  #Define the data and tidy the data
+  dat2 <- broom::tidy(model_name) %>%
+    mutate(nice_strat = sub('.*=', '', strata)) %>%
+    mutate(strata_state = paste(paste0(nice_strat,":"),state))
+  
   if(!(col_palette %in% grDevices::hcl.pals())) {
     stop("The color palette must be one of the hcl.pals()")
   }
   #Set outcome colors
   if(is.null(outcome_colors)) {
     outcome_colors <- grDevices::hcl.colors(length(unique((dat2 %>%
-                                            dplyr::filter(state != "(s0)"))$state)), col_palette)
+              dplyr::filter(state != "(s0)"))$strata_state)), col_palette)
   }
   #Make conf_int correct
   if(conf_int != TRUE & conf_int != FALSE) {
@@ -70,32 +74,26 @@ ci_graph <- function(model_name, line_size = 1, outcome_colors=NULL, col_palette
   if(conf_int_alpha > 1 | conf_int_alpha < 0) {
     stop("Alpha must be a number between 0 and 1")
   }
-  #Make into a factor when told to
-  if(!is.null(factor_order)) {
-    new_fac <- c("(s0)")
-    new_fac <- c(new_fac,factor_order)
-    dat2$state <- factor(dat2$state, levels = new_fac, ordered = TRUE)
-  }
-  #Make the plot
+  #Graph it
   if(conf_int == TRUE) {
-    dat2 %>% dplyr::filter(state != "(s0)") %>% ggplot2::ggplot() +
-      ggplot2::geom_step(ggplot2::aes(x = time, y = estimate, color = state, linetype = state),
-                         size = line_size) +
-      ggplot2::geom_ribbon(ggplot2::aes(x = time, y = estimate, fill = state, ymin = conf.low, ymax = conf.high),
-                  alpha = conf_int_alpha) +
-      ggplot2::theme_classic() +  ggplot2::theme(legend.position="bottom") +
-      ggplot2::labs(x = xlab_name, y = ylab_name) +
-      ggplot2::scale_color_manual(name = "Outcome", values = outcome_colors) +
-      ggplot2::scale_fill_manual(name = "Outcome", values = outcome_colors) +
-      ggplot2::scale_linetype_discrete(name = "Outcome") +
+  dat2 %>% dplyr::filter(state != "(s0)") %>% ggplot2::ggplot() +
+    ggplot2::geom_step(ggplot2::aes(x = time, y = estimate, color = strata_state, linetype = strata_state),
+                       size = line_size) +
+    ggplot2::geom_ribbon(ggplot2::aes(x = time, y = estimate, fill = strata_state,
+                                      ymin = conf.low, ymax = conf.high),alpha = conf_int_alpha) +
+    ggplot2::theme_classic() +  ggplot2::theme(legend.position="bottom") +
+    ggplot2::labs(x = xlab_name, y = ylab_name) +
+    ggplot2::scale_color_manual(name = "Outcome", values = outcome_colors) +
+    ggplot2::scale_fill_manual(name = "Outcome", values = outcome_colors) +
+    ggplot2::scale_linetype_discrete(name = "Outcome") +
       ggplot2::theme(legend.position = "inside",
                      legend.position.inside = c(0.15,0.8),
                      legend.title = ggplot2::element_text(size = legend_title_text_size), 
                      legend.text = ggplot2::element_text(size = legend_text_size),
                      legend.key.size = grid::unit(legend_size, 'cm'))
   } else {
-   dat2 %>% dplyr::filter(state != "(s0)") %>% ggplot2::ggplot() +
-      ggplot2::geom_step(ggplot2::aes(x = time, y = estimate, color = state, linetype = state),
+    dat2 %>% dplyr::filter(state != "(s0)") %>% ggplot2::ggplot() +
+      ggplot2::geom_step(ggplot2::aes(x = time, y = estimate, color = strata_state, linetype = strata_state),
                          size = line_size) +
       ggplot2::theme_classic() +  ggplot2::theme(legend.position="bottom") +
       ggplot2::labs(x = xlab_name, y = ylab_name) +
